b46350b259bad02a5f7a545830a21cee8d912ba7
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-02-12 16:33:23 +0100

    add logic to ExpensesSummary component

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,11 +1,12 @@
 import { View } from "react-native";
+
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput({ expenses }) {
+function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View>
-      <ExpensesSummary />
+      <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
       <ExpensesList />
     </View>
   );
--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,10 +1,14 @@
 import { View, Text } from "react-native";
 
-function ExpensesSummary() {
+function ExpensesSummary({ expenses, periodName }) {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0); // reduce allows you to combine multiple values in an array into a single value
+
   return (
     <View>
-      <Text>Last 7 days</Text>
-      <Text>$177</Text>
+      <Text>{periodName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
     </View>
   );
 }
