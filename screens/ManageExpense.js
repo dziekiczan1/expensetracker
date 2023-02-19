77c954619774b756876487cc214260f7dd2c395f
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-02-19 09:37:01 +0100

    add styling to ManageExpense screen

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,7 +1,11 @@
-import { Text } from "react-native";
+import { Text, StyleSheet } from "react-native";
 
 function ManageExpense() {
   return <Text>ManageExpense Screen</Text>;
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  container: {},
+});
