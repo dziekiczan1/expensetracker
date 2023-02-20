c6cc905132873b2466cb1fa108729eaf0b072740
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-02-20 17:23:58 +0100

    remove a line of style from View element

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -19,7 +19,7 @@ function ExpenseItem({ id, description, amount, date }) {
       style={({ pressed }) => pressed && styles.pressed}
     >
       <View style={styles.expenseItem}>
-        <View style={styles.expenseItem}>
+        <View>
           <Text style={[styles.textBase, styles.description]}>
             {description}
           </Text>
@@ -36,41 +36,41 @@ function ExpenseItem({ id, description, amount, date }) {
 export default ExpenseItem;
 
 const styles = StyleSheet.create({
-  pressed: { opacity: 0.75 },
-  container: {
-    expenseItem: {
-      padding: 12,
-      marginVertical: 8,
-      backgroundColor: GlobalStyles.colors.primary500,
-      flexDirection: "row",
-      justifyContent: "space-between",
-      borderRadius: 6,
-      elevation: 3,
-      shadowColor: GlobalStyles.colors.gray500,
-      shadowRadius: 4,
-      shadowOffset: { width: 1, height: 1 },
-      shadowOpacity: 0.4,
-    },
-    textBase: {
-      color: GlobalStyles.colors.primary50,
-    },
-    description: {
-      fontSize: 16,
-      marginBottom: 4,
-      fontWeight: "bold",
-    },
-    amountContainer: {
-      horizontalPadding: 12,
-      paddingVertical: 4,
-      backgroundColor: "white",
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: 4,
-      minWidth: 80,
-    },
-    amount: {
-      color: GlobalStyles.colors.primary500,
-      fontWeight: "bold",
-    },
+  pressed: {
+    opacity: 0.75,
+  },
+  expenseItem: {
+    padding: 12,
+    marginVertical: 8,
+    backgroundColor: GlobalStyles.colors.primary500,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderRadius: 6,
+    elevation: 3,
+    shadowColor: GlobalStyles.colors.gray500,
+    shadowRadius: 4,
+    shadowOffset: { width: 1, height: 1 },
+    shadowOpacity: 0.4,
+  },
+  textBase: {
+    color: GlobalStyles.colors.primary50,
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 4,
+    fontWeight: "bold",
+  },
+  amountContainer: {
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 4,
+    minWidth: 80,
+  },
+  amount: {
+    color: GlobalStyles.colors.primary500,
+    fontWeight: "bold",
   },
 });
