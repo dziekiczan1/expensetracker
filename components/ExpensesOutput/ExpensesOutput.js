f0c1e4bed6dd80627d40e7ed2d711993e06483bc
dzekiczan1 <p.rzadkowolski@gmail.com>
2023-02-21 16:40:24 +0100

    add updateExpense function and ADD case in expenses-context

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,27 +4,6 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const MY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of smth",
-    amount: 19.99,
-    date: new Date("2023-02-15"),
-  },
-  {
-    id: "e2",
-    description: "A pair of smth else",
-    amount: 41.99,
-    date: new Date("2023-01-15"),
-  },
-  {
-    id: "e3",
-    description: "A pair of smth else else",
-    amount: 121.99,
-    date: new Date("2022-06-15"),
-  },
-];
-
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
     <View style={styles.container}>
